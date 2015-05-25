9671ec20d91209b2976edad0815e71daf5797647
mgaldino <mcz.fea@gmail.com>
2015-05-25 19:09:56 -0300

    Urls

---
+++ contrib.R
@@ -0,0 +1,24 @@
+
+
+
+
+## total dep fed 7140
+# dep est 17010
+# senador 185
+# primeoiro suplnete senador
+# segundo suplente senador
+
+url1 <- "http://inter01.tse.jus.br/divulga-cand-2014/eleicao/2014/idEleicao/143/cargo/5/UF/RO/candidato/220000000189"
+url2 <-  "http://inter01.tse.jus.br/divulga-cand-2014/eleicao/2014/idEleicao/143/cargo/5/UF/PR/candidato/160000000636"
+
+url_uf <- "http://inter01.tse.jus.br/divulga-cand-2014/eleicao/2014/UF/AC/candidatos/cargo/3"
+http://inter01.tse.jus.br/divulga-cand-2014/eleicao/2014/UF/AL/candidatos/cargo/3
+
+# deputado fed
+# http://inter01.tse.jus.br/divulga-cand-2014/eleicao/2014/UF/AL/candidatos/cargo/6
+
+# senador
+# http://inter01.tse.jus.br/divulga-cand-2014/eleicao/2014/UF/AL/candidatos/cargo/5
+
+# dep estad
+# http://inter01.tse.jus.br/divulga-cand-2014/eleicao/2014/UF/AL/candidatos/cargo/7
