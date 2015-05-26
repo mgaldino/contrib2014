1b906aee2cbe4f76407c69f1503abcb7c7720b6a
mgaldino <mcz.fea@gmail.com>
2015-05-26 14:11:33 -0300

    Tema da Transparencia Brasil pro ggplot

---
+++ theme_tb.R
@@ -0,0 +1,21 @@
+## Theme for Tranasparência Brasil
+
+theme_tb <- function(base_size = 12, base_family = "calibri", legenda="none") {
+   theme(
+     line = element_line(),
+     axis.title = element_text(size=base_size),
+     axis.text = element_text(size=14),
+     axis.ticks = element_blank(),
+     axis.line = element_blank(),
+     legend.background = element_rect(),
+     legend.position = legenda,
+     legend.direction = "horizontal",
+     legend.box = "vertical",
+     panel.grid = element_line(colour = NULL),
+     panel.grid.major = element_blank(),
+     panel.grid.minor = element_blank(),
+     panel.background = element_blank(),
+     # unfortunately, can't mimic subtitles
+     strip.background=element_rect())
+}
+
